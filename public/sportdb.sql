4a80cf160598010e39e334334a4fb0e4599d218c
zengning <0380008937@hxct.com>
2019-09-04 15:35:11 +0800

    update sql

--- public/sportdb.sql
+++ public/sportdb.sql
@@ -11,11 +11,37 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 02/09/2019 16:39:08
+ Date: 04/09/2019 15:34:02
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for staff
+-- ----------------------------
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE `staff`  (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `storeid` int(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `account`(`account`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of staff
+-- ----------------------------
+INSERT INTO `staff` VALUES (1, 'john', '13666666666', '123456', '销售', 2);
+INSERT INTO `staff` VALUES (5, 'mary', '13666666667', '123456', '教练', 2);
+INSERT INTO `staff` VALUES (7, 'mary', '13166666667', '123456', '教练', 2);
+INSERT INTO `staff` VALUES (8, 'mary', '13126666667', '123456', '教练', 2);
+INSERT INTO `staff` VALUES (9, 'mary', '13126646667', '123456', '教练', 2);
+INSERT INTO `staff` VALUES (10, 'mary', '13126646867', '123456', '教练', 2);
+INSERT INTO `staff` VALUES (11, 'mary', '13126646861', '123456', '教练', 1);
 
 -- ----------------------------
 -- Table structure for storefiles
@@ -43,12 +69,28 @@ CREATE TABLE `storeinfos`  (
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `pid` int(10) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of storeinfos
 -- ----------------------------
 INSERT INTO `storeinfos` VALUES (2, 'admin1', '123456', '2号店', 2);
+
+-- ----------------------------
+-- Table structure for type
+-- ----------------------------
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE `type`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of type
+-- ----------------------------
+INSERT INTO `type` VALUES (1, '销售');
+INSERT INTO `type` VALUES (2, '教练');
 
 -- ----------------------------
 -- Table structure for usr
@@ -59,7 +101,7 @@ CREATE TABLE `usr`  (
   `user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `pwd` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of usr
